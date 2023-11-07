40bd1b0fb98b86981dda2f029376f245a69de728
Rohan2806 <118652086+Rohan2806@users.noreply.github.com>
2023-11-07 09:14:43 +0000

    done

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,39 @@
+let value= 3
+let negvalue= -value;
+
+/*
+ console.log(negvalue);
+ console.log(3+3);
+ console.log(3-3);
+ console.log(3*3);
+ console.log(3/3);
+ console.log(3**3);
+ console.log(3%3);
+*/
+ 
+ 
+let str1 = "hello "
+let str2 = "Rohan"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1"+3);
+// console.log(1+"3");
+// console.log(1+"3"+5);
+// console.log(1+5+"3");
+
+// console.log( 2 + 3 * 4 % 7);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2, num3
+num1 = num2 = num3 = 3 + 5
+//  reduces code readability hence not much recommended
+
+let Gamecounter=100;
+++Gamecounter;
+Gamecounter++;
+console.log(Gamecounter);
+
