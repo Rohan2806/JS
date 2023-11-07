23ef2b98fb29a5f19594da81968569e378936ae7
Rohan2806 <118652086+Rohan2806@users.noreply.github.com>
2023-11-07 11:59:00 +0000

    done

--- 01_basics/dataType-Summary.js
+++ 01_basics/dataType-Summary.js
@@ -9,29 +9,53 @@
 
   */
 
-  const score = 100;
-  const scoreValue = 110.5;
+  // const score = 100;
+  // const scoreValue = 110.5;
 
-  const isLoggedIn = true;
-  const OutsideTemp = null;
+  // const isLoggedIn = true;
+  // const OutsideTemp = null;
   
-  let userEmail;
+  // let userEmail;
   
-  const Id = Symbol("1234");
-  const AnotherId = Symbol("1234");
+  // const Id = Symbol("1234");
+  // const AnotherId = Symbol("1234");
 
-  const BigNumber = 146876239129237912n;
-  console.log(Id === AnotherId);
+  // const BigNumber = 146876239129237912n;
+  // console.log(Id === AnotherId);
 
 
-  const hero = ["Ironman" , "Captian" , "Hulk" , "Dr. Strange"];
+  // const hero = ["Ironman" , "Captian" , "Hulk" , "Dr. Strange"];
 
-  let myObj ={
-    name: "Rohan",
-    age: 18,
-    city: Dewas
+  // let myObj ={
+  //   name: "Rohan",
+  //   age: 18,
+  //   city: Dewas
+  // }
+
+  // let MyFunc1 = function name(String) {
+  //   console.log("Hello");
+  // }
+
+  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  //
+
+  // Stack (Primitive), Heap (Non-Primitive)
+
+  let MyChannelName = "rohanpalasedotcom"
+
+  let AnotherName = MyChannelName;
+  AnotherName = "Rohanrathoredotcom"
+
+  console.log(MyChannelName);
+  console.log(AnotherName);
+  
+  let User1 = {
+    email : "ronit@gogo.com",
+    age : 21,
+    upi: "roni@ybl"  
   }
-
-  let MyFunc1 = function name(String) {
-    console.log("Hello");
-  }
+  let User2 = User1
+  
+  User2.email = "JaiShreeRam.com"
+  
+  console.log(User2);
+  console.log(User1);
