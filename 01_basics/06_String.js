b6883b060fda515aa29c83e72cefc41a5fec2157
Rohan2806 <118652086+Rohan2806@users.noreply.github.com>
2023-11-07 20:39:50 +0000

    done

---
+++ 01_basics/06_String.js
@@ -0,0 +1,19 @@
+const name = "rohan";
+const repocount = 30;
+
+// console.log(name + repocount + " value");
+
+// below is the example of String interpolation and we can also use methods like toUpperCase, toLowerCase etc.
+
+// console.log(`hello my name is ${name} and my repo count is ${repocount}.`);
+
+const gameName = new String('Rohan_Palase');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase());
+
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('a'));
