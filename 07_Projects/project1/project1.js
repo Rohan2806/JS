25de3171cccf29b20ed2cd2f0705d19f065ef43d
Rohan2806 <118652086+Rohan2806@users.noreply.github.com>
2023-11-30 18:42:03 +0000

    done

---
+++ 07_Projects/project1/project1.js
@@ -0,0 +1,22 @@
+const Buttons = document.querySelectorAll(`.button`);
+const Body = document.querySelector(`body`);
+
+Buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (event) {
+    console.log(event);
+    console.log(event.target);
+    if (event.target.id === 'grey') {
+      Body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'white') {
+      Body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'blue') {
+      Body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'yellow') {
+      Body.style.backgroundColor = event.target.id;
+    }
+  });
+});
